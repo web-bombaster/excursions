72aaef9c89039834c83ccb3839d900f1984fcef5
web-bombaster <info@sajt-vizitka-nedorogo.ru>
2022-09-07 23:59:25 +0300

    first commit

---
+++ src/libs/libs-includ.js
@@ -0,0 +1,10 @@
+// Сначала инклюдим необходимые библтотеки
+// include 'jquery.magnific-popup.js'
+// include 'slick.js'
+// Анимация отсчёта на jquery-spincrement
+// include 'jquery.spincrement.min.js'
+
+
+// В конце импортируем common.min.js с инициализацией подключенных библиотек
+// и с подключением своих скриптов
+// include '../js/common.min.js'
